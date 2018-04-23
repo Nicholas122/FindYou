97484a0d144f413777b663034153b70f150d1d66
Nicholas Gritsuk <gritsuk57@gmail.com>
2018-04-23 22:29:25 +0300

    Fix AppUserProvider

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -26,7 +26,7 @@ server '31.131.21.158', user: 'deployer', roles: %w{app web db}
 # Feel free to add new variables to customise your setup.
 set :deploy_to, '/var/www/server/findyou/'
 # Default branch is :master
-set :branch, :'upgrade'
+set :branch, :'updade'
 
 #Custom SSH Options
 # ==================
